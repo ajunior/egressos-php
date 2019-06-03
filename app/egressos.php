e92e3224e3e9d4971d22270834e022cffbf9986e
A Junior <jr@ieee.org>
2019-06-03 13:47:35 -0300

    update app

--- app/egressos.php
+++ app/egressos.php
@@ -12,5 +12,24 @@ if(!isset($_SESSION['userid'])) {
 
 ?>
 
-<p>
-    <a href="logout.php" class="btn btn-outline-primary">Sair</a></p>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <title>Egressos IFPB</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="public/css/style.css">
+</head>
+<body>
+  <nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand">Egressos</a>
+    <form class="form-inline">
+      <a href="logout.php" class="btn btn-outline-danger my-2 my-sm-0" type="submit">Sair</a>
+    </form>
+  </nav>
+
+  <section id="lista-egressos">
+
+  </section>
+</body>
+</html>
